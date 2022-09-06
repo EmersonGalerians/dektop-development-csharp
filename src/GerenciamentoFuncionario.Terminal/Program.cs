33c07afd5f5532f8adfecee8da93fd8859572ce2
EmersonGalerians <106710578+EmersonGalerians@users.noreply.github.com>
2022-09-06 19:23:16 -0300

    att

---
+++ src/GerenciamentoFuncionario.Terminal/Program.cs
@@ -0,0 +1,21 @@
+﻿using Gerenciamento_Funcionario.AcessData;
+using System;
+
+namespace GerenciamentoFuncionario.Terminal
+{
+    class Program
+    {
+        static void Main()
+        {
+            var provedorFuncionario = new FuncionarioProvedorDados();
+            var funcionarios = provedorFuncionario.CarregaFuncionarios();
+
+            foreach (var funcionario in funcionarios) {
+                Console.WriteLine($"{funcionario.Id} {funcionario.NomeCompleto}");
+                Console.WriteLine("\n\n");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
